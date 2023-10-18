918f1c8f6978d193c00c02c7c7dd744f480b4af7
jramicl <cljrami@gmail.com>
2023-10-18 19:31:18 -0300

    prueba

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -401,7 +401,10 @@
         </div>
     </section>
     <!-- ========== FOOTER ========== -->
-
+    !<div class="form-floating">
+        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+        <label for="floatingTextarea">Comments</label>
+    </div>
 
 
     <!-- FOOTER SCRIPTS -->
